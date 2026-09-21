b11d2e49d2a60574edbc3745e7c35fc6e98adf77
hzy <2713334990@qq.com>
2026-09-21 13:35:14 +0800

    add: C++函数重载与模板特化优先级测试

--- warnings_of_temp.cpp
+++ warnings_of_temp.cpp
@@ -22,7 +22,19 @@ class girl
 template <typename T1>
 T1 add(T1& t)
 {
+    cout<<"普通模板"<<endl;
     return ++t;
+}
+template<>             //具体化
+int add<int>(int& a)
+{
+    cout<<"具体化模板"<<endl;
+    return ++a;
+}
+int add(int& a)
+{
+    cout<<"普通函数"<<endl;
+    return ++a;
 }
 template <typename T,typename a,typename b>
 T add(T& t,a& a1,b& b1)      //也可以用模板 去重载
@@ -30,11 +42,7 @@ T add(T& t,a& a1,b& b1)      //也可以用模板 去重载
     cout<<t<<"   "<<a1+b1<<endl;
     return t;
 }
-template<>
-int add<int>(int& a)
-{
-    return ++a;
-}
+
 template <typename T> void swa(girl<T>&);
 template<typename T>
 void swa(girl<T>& g)
@@ -50,6 +58,7 @@ void swap1(T& t1,T& t2)
 }
 // 特化：类型必须写死（全特化），不能留模板参数（部分特化）。
 // 重载：同名函数，接受更具体的类型（如 girl<T>），编译器会优先匹配更具体的版本。
+// 普通函数(重载)>具体化模板>普通模板 
 
 template<typename T1>
 void swap1(girl<T1> &g1,girl<T1> &g2)
@@ -68,7 +77,10 @@ int main()
     swap1(g,g2);    //交换
     g2.show();
     g.show();
-
+    int a=8;
+    add(a);
+    add<>(a);
+    add(gg);
 
 
 
