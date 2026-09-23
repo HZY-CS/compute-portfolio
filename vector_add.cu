6bb1d10601b3b3213f48210761b14c786978e9de
hzy <2713334990@qq.com>
2026-09-23 23:12:43 +0800

    add: 完成 CUDA vector_add 控制变量实验及报告

--- vector_add.cu
+++ vector_add.cu
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <cuda_runtime.h>
 
+// CUDA 错误检查宏
 #define CHECK(call) \
 { \
     const cudaError_t error = call; \
@@ -13,6 +14,7 @@
     } \
 }
 
+// CUDA Kernel：每个线程计算一个元素
 __global__ void vectorAdd(const float *A, const float *B, float *C, int numElements) {
     int i = blockDim.x * blockIdx.x + threadIdx.x;
     if (i < numElements) {
@@ -21,10 +23,12 @@ __global__ void vectorAdd(const float *A, const float *B, float *C, int numEleme
 }
 
 int main() {
-    int numElements = 50000;
+    // 修改这里的数字做实验A
+    int numElements = 1000000; 
     size_t size = numElements * sizeof(float);
     std::cout << "[Vector addition of " << numElements << " elements]" << std::endl;
 
+    // 1. 分配主机内存
     float *h_A = (float *)malloc(size);
     float *h_B = (float *)malloc(size);
     float *h_C = (float *)malloc(size);
@@ -34,21 +38,27 @@ int main() {
         h_B[i] = rand() / (float)RAND_MAX;
     }
 
+    // 2. 分配设备（GPU）内存
     float *d_A = NULL, *d_B = NULL, *d_C = NULL;
     CHECK(cudaMalloc((void **)&d_A, size));
     CHECK(cudaMalloc((void **)&d_B, size));
     CHECK(cudaMalloc((void **)&d_C, size));
 
+    // 3. 数据从主机拷贝到设备
     CHECK(cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice));
     CHECK(cudaMemcpy(d_B, h_B, size, cudaMemcpyHostToDevice));
 
-    int threadsPerBlock = 256;
+    // 4. 启动 Kernel
+    // 修改这里的数字做实验B
+    int threadsPerBlock = 512; 
     int blocksPerGrid = (numElements + threadsPerBlock - 1) / threadsPerBlock;
     std::cout << "CUDA kernel launch with " << blocksPerGrid << " blocks of " << threadsPerBlock << " threads" << std::endl;
     vectorAdd<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_B, d_C, numElements);
 
+    // 5. 结果从设备拷贝回主机
     CHECK(cudaMemcpy(h_C, d_C, size, cudaMemcpyDeviceToHost));
 
+    // 6. 验证结果
     for (int i = 0; i < numElements; ++i) {
         if (fabs(h_A[i] + h_B[i] - h_C[i]) > 1e-5) {
             std::cerr << "Result verification failed at element " << i << "!" << std::endl;
@@ -57,6 +67,7 @@ int main() {
     }
     std::cout << "Test PASSED" << std::endl;
 
+    // 7. 释放内存
     free(h_A); free(h_B); free(h_C);
     CHECK(cudaFree(d_A)); CHECK(cudaFree(d_B)); CHECK(cudaFree(d_C));
     return 0;
